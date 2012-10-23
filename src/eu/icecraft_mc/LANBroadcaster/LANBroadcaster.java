32bd822193b1739b2ba49bdb89a4b4c2743aa1ad
ruan <ruan9422@gmail.com>
2012-10-23 15:36:38 +0200

    automatically detect LAN ip, better error handling, cleanup

--- src/eu/icecraft_mc/LANBroadcaster/LANBroadcaster.java
+++ src/eu/icecraft_mc/LANBroadcaster/LANBroadcaster.java
@@ -3,7 +3,7 @@ package eu.icecraft_mc.LANBroadcaster;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LANBroadcaster extends JavaPlugin {
-    public final Thread broadcastThread = new LANBroadcasterThread(this);
+    public Thread broadcastThread;
 
     @Override
     public void onDisable() {
@@ -12,6 +12,7 @@ public class LANBroadcaster extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        broadcastThread = new LANBroadcasterThread(this);
         broadcastThread.start();
     }
 }
