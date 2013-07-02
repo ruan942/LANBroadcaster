c3c435ef6fb294df54a53b0cb71f38088a0cc941
Ruan <ruany@users.noreply.github.com>
2013-07-02 21:23:48 +0200

    update for 1.6 protocol, clean up, add description to plugin.yml

--- src/eu/icecraft_mc/LANBroadcaster/LANBroadcasterThread.java
+++ src/eu/icecraft_mc/LANBroadcaster/LANBroadcasterThread.java
@@ -4,65 +4,63 @@ import java.net.*;
 import java.util.Enumeration;
 
 public class LANBroadcasterThread extends Thread {
-    private LANBroadcaster parent;
+    private LANBroadcaster plugin;
     private DatagramSocket socket;
-    public boolean running = true;
 
-    public LANBroadcasterThread(LANBroadcaster plugin) {
+    public LANBroadcasterThread(LANBroadcaster parent) {
         super("LANBroadcaster");
-        this.parent = plugin;
+        this.plugin = parent;
         try {
             socket = new DatagramSocket();
             socket.setSoTimeout(3000);
         } catch (Exception e) {
             e.printStackTrace();
-            parent.getLogger().severe("Host does not support datagram sockets; disabling.");
-            parent.getPluginLoader().disablePlugin(parent);
-            running = false;
+            plugin.getLogger().severe("Host does not support datagram sockets; disabling.");
+            plugin.getPluginLoader().disablePlugin(plugin);
         }
-    }
-
-    @Override
-    public void interrupt() {
-        super.interrupt();
-        running = false;
-        socket.close();
     }
 
     @Override
     public void run() {
         try {
-            String motd = parent.getServer().getMotd();
-            String serverLanIP = getLanIP() + ":" + parent.getServer().getPort();
-            byte[] ad = ("[MOTD]" + motd + "[/MOTD][AD]" + serverLanIP + "[/AD]").getBytes();
+            byte[] ad = getAd();
             DatagramPacket packet = new DatagramPacket(ad, ad.length, InetAddress.getByName("224.0.2.60"), 4445);
-            parent.getLogger().info("Broadcasting " + serverLanIP + " over LAN.");
-            while (!isInterrupted() && running) {
-                try {
-                    socket.send(packet);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    break;
-                }
+            while (!isInterrupted()) {
+                socket.send(packet);
                 try {
                     sleep(1500L);
-                } catch (InterruptedException e) {}
+                } catch (InterruptedException e) {
+                    break;
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        socket.close();
     }
 
-    private String getLanIP() throws Exception {
-        if (!parent.getServer().getIp().equals("")) return parent.getServer().getIp();
+    private byte[] getAd() {
+        String motd = plugin.getServer().getMotd();
+        String ip = getLanIP();
+        int port = plugin.getServer().getPort();
+        String ad = ip + ":" + port;
+        if (isBukkit1_6()) {
+            ad = String.valueOf(port);
+            plugin.getLogger().info("Broadcasting server with port " + ad + " over LAN.");
+        } else {
+            plugin.getLogger().info("Broadcasting " + ip + " over LAN.");
+        }
+        byte[] adBytes = ("[MOTD]" + motd + "[/MOTD][AD]" + ad + "[/AD]").getBytes();
+        return adBytes;
+    }
 
+    private String getLanIP() {
+        if (!plugin.getServer().getIp().equals("")) return plugin.getServer().getIp();
         try {
             Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-
             while (interfaces.hasMoreElements()) {
                 NetworkInterface iface = interfaces.nextElement();
                 Enumeration<InetAddress> addresses = iface.getInetAddresses();
-
                 while (addresses.hasMoreElements()) {
                     InetAddress address = addresses.nextElement();
                     if (address instanceof Inet4Address && !address.isLoopbackAddress()) return address.getHostAddress();
@@ -71,8 +69,21 @@ public class LANBroadcasterThread extends Thread {
             throw new Exception("No usable IPv4 non-loopback address found");
         } catch (Exception e) {
             e.printStackTrace();
-            parent.getLogger().severe("Could not automatically detect LAN IP, please set server-ip in server.properties.");
-            return InetAddress.getLocalHost().getHostAddress();
+            plugin.getLogger().severe("Could not automatically detect LAN IP, please set server-ip in server.properties.");
+            try {
+                return InetAddress.getLocalHost().getHostAddress();
+            } catch (UnknownHostException ex) {
+                return "End of the world";
+            }
+        }
+    }
+
+    private boolean isBukkit1_6() {
+        try {
+            Class.forName("org.bukkit.entity.Horse"); // just another class
+            return true;
+        } catch (Throwable ex) {
+            return false;
         }
     }
 }
